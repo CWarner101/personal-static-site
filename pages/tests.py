c7598b008b96c2ef9bd3800b6ce251eabcffa558
CWarner101 <con.war10@gmail.com>
2024-01-22 19:29:19 -0500

    Add test cases covering url at correct location, url available by name, correct template name, and correct template content (to be added to later) for home/resume, projects, and contact page.

--- pages/tests.py
+++ pages/tests.py
@@ -7,3 +7,80 @@ from django.test import SimpleTestCase
 from django.urls import reverse
 
 # Create your tests here.
+
+class HomepageTest(SimpleTestCase):
+    """Test home/resume page"""
+
+    def test_url_exists_at_correct_location(self):
+        """Test url exists at correct location"""
+
+        responce = self.client.get("/")
+        self.assertEqual(responce.status_code, 200)
+
+    def test_url_available_by_name(self):
+        """Test url available by name"""
+        responce = self.client.get(reverse("home"))
+        self.assertEqual(responce.status_code, 200)
+
+    def test_template_name_correct(self):
+        "Test template name correct"
+        responce = self.client.get(reverse("home"))
+        self.assertTemplateUsed(responce, "home.html")
+
+    def test_template_content(self):
+        """Test template content"""
+        # responce = self.client.get(reverse("home"))
+        # self.assertContains(responce, "TODO: Add content")
+        pass
+
+
+class ProjectspageTest(SimpleTestCase):
+    """Test projects page"""
+
+    def test_url_exists_at_correct_location(self):
+        """Test url exists at correct location"""
+
+        responce = self.client.get("/projects/")
+        self.assertEqual(responce.status_code, 200)
+
+    def test_url_available_by_name(self):
+        """Test url available by name"""
+        responce = self.client.get(reverse("projects"))
+        self.assertEqual(responce.status_code, 200)
+
+    def test_template_name_correct(self):
+        "Test template name correct"
+        responce = self.client.get(reverse("projects"))
+        self.assertTemplateUsed(responce, "projects.html")
+
+    def test_template_content(self):
+        """Test template content"""
+        # responce = self.client.get(reverse("projects"))
+        # self.assertContains(responce, "TODO: Add content")
+        pass
+
+
+class ContactpageTest(SimpleTestCase):
+    """Test projects page"""
+
+    def test_url_exists_at_correct_location(self):
+        """Test url exists at correct location"""
+
+        responce = self.client.get("/contact/")
+        self.assertEqual(responce.status_code, 200)
+
+    def test_url_available_by_name(self):
+        """Test url available by name"""
+        responce = self.client.get(reverse("contact"))
+        self.assertEqual(responce.status_code, 200)
+
+    def test_template_name_correct(self):
+        "Test template name correct"
+        responce = self.client.get(reverse("contact"))
+        self.assertTemplateUsed(responce, "contact.html")
+
+    def test_template_content(self):
+        """Test template content"""
+        # responce = self.client.get(reverse("projects"))
+        # self.assertContains(responce, "TODO: Add content")
+        pass
